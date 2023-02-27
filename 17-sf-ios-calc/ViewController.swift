ce07e9e7e3384d15583fd7748eba8fc1bafecd6e
Sergey Polikarpov <xpalik@gmail.com>
2023-02-27 16:00:04 +0300

    Initial Commit

---
+++ 17-sf-ios-calc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  17-sf-ios-calc
+//
+//  Created by Сергей Поликарпов on 27.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
